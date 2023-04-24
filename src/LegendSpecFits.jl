194f283929ab343fbd2a3eef0926ff2cc913438a
Oliver Schulz <oschulz@mpp.mpg.de>
2023-04-24 14:24:59 +0200

    Make Optim a required dependency for now

--- src/LegendSpecFits.jl
+++ src/LegendSpecFits.jl
@@ -51,7 +51,6 @@ include("precompile.jl")
 function __init__()
     @static if !isdefined(Base, :get_extension)
         @require BAT = "c0cd4b16-88b7-57fa-983b-ab80aecada7e" include("../ext/LegendSpecFitsBATExt.jl")
-        @require Optim = "429524aa-4258-5aef-a3af-852621145aeb" include("../ext/LegendSpecFitsOptimExt.jl")
     end
 end
 
