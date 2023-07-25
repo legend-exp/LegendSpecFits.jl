aaff59cc7cce8310458cded32bcf32fab39cbbfc
Florian Henkes <florian.henkes@outlook.com>
2023-07-25 16:44:38 +0200

    updated extensions and recipes

--- ext/LegendSpecFitsBATExt.jl
+++ ext/LegendSpecFitsBATExt.jl
@@ -7,7 +7,6 @@ isdefined(Base, :get_extension) ? (using BAT) : (using ..BAT)
 using Distributions
 
 #TODO get this running, doesn't work at the moment
-get_distribution_transform(d::Distribution, pprior::NamedTupleDist) = BAT.DistributionTransform(d, pprior)
-export get_distribution_transform
+LegendSpecFits.get_distribution_transform(d::Distribution, pprior::NamedTupleDist) = BAT.DistributionTransform(d, pprior)
 
 end # module LegendSpecFitsBATExt
--- ext/LegendSpecFitsRecipesBaseExt.jl
+++ ext/LegendSpecFitsRecipesBaseExt.jl
@@ -4,6 +4,7 @@ module LegendSpecFitsRecipesBaseExt
 
 using RecipesBase
 using Unitful, Formatting
+using StatsBase, LinearAlgebra
 
 # @recipe function f(x::Vector{T}, cuts::NamedTuple{(:low, :high, :max), Tuple{T, T, T}}) where T<:Unitful.RealOrRealQuantity
 @recipe function f(report::NamedTuple{(:f_fit, :μ, :μ_err, :σ, :σ_err, :n), Tuple{Q, T, T, T, T, Int64}}, x::Vector{T}, cuts::NamedTuple{(:low, :high, :max), Tuple{T, T, T}}) where {Q <: Function, T <: Unitful.RealOrRealQuantity}
@@ -27,7 +28,7 @@ using Unitful, Formatting
 end
 
 @recipe function f(report:: NamedTuple{(:rt, :min_enc, :enc_grid_rt, :enc, :enc_err), Tuple{Q, T, Vector{Quantity{<:T}}, Vector{T}, Vector{T}}}) where {T <: Real, Q<:Quantity{T}}
-    xlabel := "Rise Time"
+    xlabel := "Rise Time (µs)"
     ylabel := "ENC (ADC)"
     grid := :true
     gridcolor := :black
@@ -55,7 +56,133 @@ end
     end
 end
 
+@recipe function f(report:: NamedTuple{(:ft, :min_fwhm, :e_grid_ft, :fwhm), Tuple{Q, T, Vector{Quantity{<:T}}, Vector{T}}}) where {T <: Real, Q<:Quantity{T}}
+    xlabel := "Flat-Top Time (µs)"
+    ylabel := "FWHM FEP (keV)"
+    grid := :true
+    gridcolor := :black
+    gridalpha := 0.2
+    gridlinewidth := 0.5
+    # xscale := :log10
+    # yscale := :log10
+    ylims := (1, 5)
+    xlims := (1, 5)
+    @series begin
+        seriestype := :scatter
+        u"µs", NoUnits
+    end
+    @series begin
+        seriestype := :scatter
+        label := "FWHM"
+        report.e_grid_ft[report.fwhm .> 0.0]*NoUnits, report.fwhm[report.fwhm .> 0.0]
+    end
+    @series begin
+        seriestype := :hline
+        label := "Min. FWHM (FT: $(report.ft))"
+        color := :red
+        linewidth := 2.5
+        [report.min_fwhm]
+    end
+end
 
+@recipe function f(report::NamedTuple{(:v, :h, :f_fit, :f_sig, :f_lowEtail, :f_bck)})
+    xlabel := "Energy (keV)"
+    ylabel := "Counts"
+    legend := :bottomright
+    yscale := :log10
+    ylims := (1, 1.2*report.f_sig(report.v.μ))
+    @series begin
+        seriestype := :stepbins
+        label := "Data"
+        bins --> :sqrt
+        LinearAlgebra.normalize(report.h, mode = :density)
+    end
+    @series begin
+        seriestype := :line
+        label := "Best Fit"
+        color := :red
+        minimum(report.h.edges[1]):0.1:maximum(report.h.edges[1]), report.f_fit
+    end
+    @series begin
+        seriestype := :line
+        label := "Signal"
+        color := :green
+        minimum(report.h.edges[1]):0.1:maximum(report.h.edges[1]), report.f_sig
+    end
+    @series begin
+        seriestype := :line
+        label := "Low-E Tail"
+        color := :blue
+        minimum(report.h.edges[1]):0.1:maximum(report.h.edges[1]), report.f_lowEtail
+    end
+    @series begin
+        seriestype := :line
+        label := "Background"
+        color := :black
+        minimum(report.h.edges[1]):0.1:maximum(report.h.edges[1]), report.f_bck
+    end
+end
+
+@recipe function f(x::Vector{T}, cut::NamedTuple{(:low, :high, :max), Tuple{T, T, T}}) where T<:Unitful.RealOrRealQuantity
+    ylabel := "Counts"
+    legend := :topright
+    xlims := (median(x) - std(x), median(x) + std(x))
+    @series begin
+        seriestype := :stephist
+        label := "Data"
+        bins --> :sqrt
+        x
+    end
+    @series begin
+        seriestype := :vline
+        label := ""
+        color := :red
+        linewidth := 1.5
+        [cut.low, cut.high]
+    end
+    @series begin
+        seriestype := :vspan
+        label := "Cut Window"
+        color := :red
+        alpha := 0.1
+        fillrange := cut.high
+        [cut.low, cut.high]
+    end
+end
+
+@recipe function f(report::NamedTuple{(:h_calsimple, :h_uncal, :c, :fep_guess, :peakhists, :peakstats)}; cal::Bool)
+    ylabel := "Counts"
+    legend := :topright
+    yscale := :log10
+    if cal
+        h = LinearAlgebra.normalize(report.h_calsimple, mode = :density)
+        xlabel := "Energy (keV)"
+        xlims := (0, 3000)
+        xticks := (0:200:3000, ["$i" for i in 0:200:3000])
+        ylims := (0.2, maximum(h.weights)*1.1)
+        fep_guess = 2614.5
+    else
+        h = LinearAlgebra.normalize(report.h_uncal, mode = :density)
+        xlabel := "Energy (ADC)"
+        xlims := (0, 1.2*report.fep_guess)
+        xticks := (0:3000:1.2*report.fep_guess, ["$i" for i in 0:3000:1.2*report.fep_guess])
+        ylims := (0.2, maximum(h.weights)*1.1)
+        fep_guess = report.fep_guess
+    end
+    @series begin
+        seriestype := :stepbins
+        label := "Energy"
+        h
+    end
+    y_vline = 0.2:1:maximum(h.weights)*1.1
+    @series begin
+        seriestype := :line
+        label := "FEP Guess"
+        color := :red
+        linewidth := 1.5
+        fill(fep_guess, length(y_vline)), y_vline
+    end
+end
 
 
 end # module LegendSpecFitsRecipesBaseExt
