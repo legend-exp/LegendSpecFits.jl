1228730e2de723c86d16865125e38a14a94b3a8b
Florian Henkes <florian.henkes@outlook.com>
2023-10-26 19:27:09 +0200

    more flexible peak fitting

--- src/specfit.jl
+++ src/specfit.jl
@@ -43,18 +43,31 @@ function estimate_single_peak_stats_th228(h::Histogram{T}) where T<:Real
     W = h.weights
     E = first(h.edges)
     peak_amplitude, peak_idx = findmax(W)
-    fwhm_idx_left = findfirst(w -> w >= (first(W) + peak_amplitude) /2, W)
-    fwhm_idx_right = findlast(w -> w >= (last(W) + peak_amplitude) /2, W)
+    fwhm_idx_left = findfirst(w -> w >= (first(W) + peak_amplitude) / 2, W)
+    fwhm_idx_right = findlast(w -> w >= (last(W) + peak_amplitude) / 2, W)
     peak_max_pos = (E[peak_idx] + E[peak_idx+1]) / 2
     peak_mid_pos = (E[fwhm_idx_right] + E[fwhm_idx_left]) / 2
     peak_pos = (peak_max_pos + peak_mid_pos) / 2.0
     peak_fwhm = (E[fwhm_idx_right] - E[fwhm_idx_left]) / 1.0
     peak_sigma = peak_fwhm * inv(2*√(2log(2)))
+    peak_fwqm = NaN
+    # make sure that peakstats have non-zero sigma and fwhm values to prevent fit priors from being zero
+    if peak_fwhm == 0
+        fwqm_idx_left = findfirst(w -> w >= (first(W) + peak_amplitude) / 4, W)
+        fwqm_idx_right = findlast(w -> w >= (last(W) + peak_amplitude) / 4, W)
+        peak_fwqm = (E[fwqm_idx_right] - E[fwqm_idx_left]) / 1.0
+        peak_sigma = peak_fwqm * inv(2*√(2log(4)))
+        peak_fwhm  = peak_sigma * 2*√(2log(2))
+    end
     #peak_area = peak_amplitude * peak_sigma * sqrt(2*π)
     mean_background = (first(W) + last(W)) / 2
     mean_background = ifelse(mean_background == 0, 0.01, mean_background)
     peak_counts = inv(0.761) * (sum(view(W,fwhm_idx_left:fwhm_idx_right)) - mean_background * peak_fwhm)
-
+    peak_counts = ifelse(peak_counts < 0.0, inv(0.761) * sum(view(W,fwhm_idx_left:fwhm_idx_right)), peak_counts)
+    if !isnan(peak_fwqm)
+        peak_counts = inv(0.904) * (sum(view(W,fwqm_idx_left:fwqm_idx_right)) - mean_background * peak_fwqm)
+        peak_counts = ifelse(peak_counts < 0.0, inv(0.904) * sum(view(W,fwqm_idx_left:fwqm_idx_right)), peak_counts)
+    end
     (
         peak_pos = peak_pos, 
         peak_fwhm = peak_fwhm,
@@ -65,22 +78,22 @@ function estimate_single_peak_stats_th228(h::Histogram{T}) where T<:Real
 end
 
 """
-    fitPeaks
-Perform a fit of the peakshape to the data in `peakhists` using the initial values in `peakstats` to the calibration lines in `th228_lines`.
+    fit_peaks(peakhists::Array, peakstats::StructArray, th228_lines::Array,; calib_type::Symbol=:th228, uncertainty::Bool=true, low_e_tail::Bool=true)
+Perform a fit of the peakshape to the data in `peakhists` using the initial values in `peakstats` to the calibration lines in `th228_lines`. 
 # Returns
     * `peak_fit_plots`: array of plots of the peak fits
     * `return_vals`: dictionary of the fit results
 """
-function fit_peaks(peakhists::Array, peakstats::StructArray, th228_lines::Array,; calib_type::Symbol=:th228)
+function fit_peaks(peakhists::Array, peakstats::StructArray, th228_lines::Array,; calib_type::Symbol=:th228, uncertainty::Bool=true, low_e_tail::Bool=true)
     if calib_type == :th228
-        return fit_peaks_th228(peakhists, peakstats, th228_lines)
+        return fit_peaks_th228(peakhists, peakstats, th228_lines,; uncertainty=uncertainty, low_e_tail=low_e_tail)
     else
         error("Calibration type not supported")
     end
 end
 export fit_peaks
 
-function fit_peaks_th228(peakhists::Array, peakstats::StructArray, th228_lines::Array{T}) where T<:Real
+function fit_peaks_th228(peakhists::Array, peakstats::StructArray, th228_lines::Array{T},; uncertainty::Bool=true, low_e_tail::Bool=true) where T<:Real
     # create return and result dicts
     result = Dict{T, NamedTuple}()
     report = Dict{T, NamedTuple}()
@@ -90,7 +103,7 @@ function fit_peaks_th228(peakhists::Array, peakstats::StructArray, th228_lines::
         h  = peakhists[i]
         ps = peakstats[i]
         # fit peak
-        result_peak, report_peak = fit_single_peak_th228(h, ps, ; uncertainty=true)
+        result_peak, report_peak = fit_single_peak_th228(h, ps, ; uncertainty=uncertainty, low_e_tail=low_e_tail)
         # save results
         result[peak] = result_peak
         report[peak] = report_peak
@@ -100,37 +113,35 @@ end
 
 
 """
-    fit_single_peak_th228(h::Histogram, ps::NamedTuple{(:peak_pos, :peak_fwhm, :peak_sigma, :peak_counts, :mean_background), NTuple{5, T}};, uncertainty::Bool=true, fixed_position::Bool=false) where T<:Real
+    fit_single_peak_th228(h::Histogram, ps::NamedTuple{(:peak_pos, :peak_fwhm, :peak_sigma, :peak_counts, :mean_background), NTuple{5, T}};, uncertainty::Bool=true, fixed_position::Bool=false, low_e_tail::Bool=true) where T<:Real
 Perform a fit of the peakshape to the data in `h` using the initial values in `ps` while using the `gamma_peakshape` with low-E tail.
-Also, FWHM is calculated from the fitted peakshape with MC error propagation. 
-The peak position can be fixed to the value in `ps` by setting `fixed_position=true`.
+Also, FWHM is calculated from the fitted peakshape with MC error propagation. The peak position can be fixed to the value in `ps` by setting `fixed_position=true`. If the low-E tail should not be fitted, it can be disabled by setting `low_e_tail=false`.
 # Returns
     * `result`: NamedTuple of the fit results containing values and errors
     * `report`: NamedTuple of the fit report which can be plotted
 """
-function fit_single_peak_th228(h::Histogram, ps::NamedTuple{(:peak_pos, :peak_fwhm, :peak_sigma, :peak_counts, :mean_background), NTuple{5, T}}; uncertainty::Bool=true, fixed_position::Bool=false) where T<:Real
-    # create pseudo priors
-    pseudo_prior = NamedTupleDist(
-        μ = Uniform(ps.peak_pos-10, ps.peak_pos+10),
+function fit_single_peak_th228(h::Histogram, ps::NamedTuple{(:peak_pos, :peak_fwhm, :peak_sigma, :peak_counts, :mean_background), NTuple{5, T}}; uncertainty::Bool=true, low_e_tail::Bool=true, fixed_position::Bool=false, pseudo_prior::NamedTupleDist=NamedTupleDist(empty = true)) where T<:Real
+    # create standard pseudo priors
+    standard_pseudo_prior = NamedTupleDist(
+        μ = ifelse(fixed_position, ConstValueDist(ps.peak_pos), Uniform(ps.peak_pos-10, ps.peak_pos+10)),
         σ = weibull_from_mx(ps.peak_sigma, 2*ps.peak_sigma),
         n = weibull_from_mx(ps.peak_counts, 2*ps.peak_counts),
         step_amplitude = weibull_from_mx(ps.mean_background, 2*ps.mean_background),
-        skew_fraction = Uniform(0.005, 0.25),
-        skew_width = LogUniform(0.001, 0.1),
+        skew_fraction = ifelse(low_e_tail, Uniform(0.005, 0.25), ConstValueDist(0.0)),
+        skew_width = ifelse(low_e_tail, LogUniform(0.001, 0.1), ConstValueDist(1.0)),
         background = weibull_from_mx(ps.mean_background, 2*ps.mean_background),
     )
-    if fixed_position
-        pseudo_prior = NamedTupleDist(
-        μ = ConstValueDist(ps.peak_pos),
-        σ = weibull_from_mx(ps.peak_sigma, 2*ps.peak_sigma),
-        n = weibull_from_mx(ps.peak_counts, 2*ps.peak_counts),
-        step_amplitude = weibull_from_mx(ps.mean_background, 2*ps.mean_background),
-        skew_fraction = Uniform(0.01, 0.25),
-        skew_width = LogUniform(0.001, 0.1),
-        background = weibull_from_mx(ps.mean_background, 2*ps.mean_background),
-        )
+    # use standard priors in case of no overwrites given
+    if !(:empty in keys(pseudo_prior))
+        # check if input overwrite prior has the same fields as the standard prior set
+        @assert all(f -> f in keys(standard_pseudo_prior), keys(standard_pseudo_prior)) "Pseudo priors can only have $(keys(standard_pseudo_prior)) as fields."
+        # replace standard priors with overwrites
+        pseudo_prior = merge(standard_pseudo_prior, pseudo_prior)
+    else
+        # take standard priors as pseudo priors with overwrites
+        pseudo_prior = standard_pseudo_prior    
     end
-
+    
     # transform back to frequency space
     f_trafo = BAT.DistributionTransform(Normal, pseudo_prior)
 
