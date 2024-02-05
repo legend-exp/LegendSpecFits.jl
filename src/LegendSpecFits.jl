44413c4e76127b45f7937ea33a692a6564cda26a
LisaSchlueter <lschlueter@lbl.gov>
2024-02-05 17:20:38 +0100

    add fast flatten

--- src/LegendSpecFits.jl
+++ src/LegendSpecFits.jl
@@ -21,6 +21,7 @@ using IntervalSets
 using InverseFunctions
 using IrrationalConstants
 using LegendDataManagement
+using LegendDataTypes: fast_flatten
 using LinearRegression
 using LsqFit
 using Optim
