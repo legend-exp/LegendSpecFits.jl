d311bedcf06d047d0b4f79719d86c2524b84c48e
LisaSchlueter <lschlueter@lbl.gov>
2024-03-12 19:15:53 +0100

    Account for units in test, add Unitful to projecttoml

--- test/test_specfit.jl
+++ test/test_specfit.jl
@@ -3,18 +3,17 @@ using LegendSpecFits
 using Test
 using LegendDataTypes: fast_flatten
 using Interpolations
+using Unitful 
+include("test_utils.jl")
 
 @testset "specfit" begin
     # load data, simple calibration 
-    include("test_utils.jl")
-
     energy_test, th228_lines = generate_mc_spectrum(200000)
     
     # simple calibration fit 
-    window_sizes =  vcat([(25.0,25.0) for _ in 1:6], (30.0,30.0))
-    result_simple, report_simple = simple_calibration(energy_test, th228_lines, window_sizes, n_bins=10000,; calib_type=:th228, quantile_perc=0.995)
+    window_sizes =  vcat([(25.0u"keV",25.0u"keV") for _ in 1:6], (30.0u"keV",30.0u"keV"))
+    result_simple, report_simple = simple_calibration(ustrip.(energy_test), th228_lines, window_sizes, n_bins=10000,; calib_type=:th228, quantile_perc=0.995)
 
     # fit 
-    result, report = fit_peaks(result_simple.peakhists, result_simple.peakstats, th228_lines,; uncertainty=true);
+    result, report = fit_peaks(result_simple.peakhists, result_simple.peakstats, ustrip.(th228_lines),; uncertainty=true,calib_type = :th228);
 end
-
