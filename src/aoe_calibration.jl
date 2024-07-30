3e0f0dec4c4ea00e1fdd8c8e2961f5b6ca50a502
Florian Henkes <florian.henkes@outlook.com>
2024-07-31 00:09:14 +0200

    Separate aoe and e expressions

--- src/aoe_calibration.jl
+++ src/aoe_calibration.jl
@@ -15,7 +15,7 @@ Fit the corrections for the AoE value of the detector.
 - `σ_scs`: Fit result for the sigma values
 - `f_σ_scs`: Fit function for the sigma values
 """
-function fit_aoe_corrections(e::Array{<:Unitful.Energy{<:Real}}, μ::Array{<:Real}, σ::Array{<:Real}; e_expression::Union{String,Symbol}="e")
+function fit_aoe_corrections(e::Array{<:Unitful.Energy{<:Real}}, μ::Array{<:Real}, σ::Array{<:Real}; aoe_expression::Union{String,Symbol}="a / e", e_expression::Union{String,Symbol}="e")
     # fit compton band mus with linear function
     μ_cut = (mean(μ) - 2*std(μ) .< μ .< mean(μ) + 2*std(μ)) .&& muncert.(μ) .> 0.0
     e, μ, σ = e[μ_cut], μ[μ_cut], σ[μ_cut]
@@ -39,9 +39,7 @@ function fit_aoe_corrections(e::Array{<:Unitful.Energy{<:Real}}, μ::Array{<:Rea
     func_σ = nothing
     func_generic_σ = nothing
     if string(f_fit_σ) == "f_aoe_sigma"
-        # func_σ = "sqrt( abs($(mvalue(result_σ.par[1]))) + abs($(mvalue(result_σ.par[2]))$(e_unit)^2) / ($e_expression)^2)"
         func_σ = "sqrt( ($(mvalue(result_σ.par[1])))^2 + ($(mvalue(result_σ.par[2]))$(e_unit))^2 / ($e_expression)^2 )" 
-        # func_generic_σ = "sqrt(abs(p[1]) + abs(p[2]) / ($e_expression)^2)"
         func_generic_σ = "sqrt( (p[1])^2 + (p[2])^2 / ($e_expression)^2 )"
     end
     result_σ = merge(result_σ, (par = par_σ, func = func_σ, func_generic = func_generic_σ, σ = σ))
@@ -49,12 +47,11 @@ function fit_aoe_corrections(e::Array{<:Unitful.Energy{<:Real}}, μ::Array{<:Rea
     @debug "Compton band σ normalization: $(result_σ.func)"
 
     # put everything together into A/E correction/normalization function 
-    aoe_str = "(a / ( ($e_expression)$e_unit^-1) )" # get aoe, but without unit. 
+    aoe_str = "($aoe_expression)" # get aoe
     func_aoe_corr = "($aoe_str - ($(result_µ.func)) ) / ($(result_σ.func))"
     func_generic_aoe_corr = "(aoe - $(result_µ.func_generic)) / $(result_σ.func_generic)"
-    func_aoe_corr_ecal = replace(func_aoe_corr, string(e_expression) => "e_cal") # function that can be used for already calibrated energies 
 
-    result = (µ_compton = result_µ, σ_compton = result_σ, compton_bands = (e = e,), func = func_aoe_corr, func_generic = func_generic_aoe_corr, func_ecal = func_aoe_corr_ecal)
+    result = (µ_compton = result_µ, σ_compton = result_σ, compton_bands = (e = e,), func = func_aoe_corr, func_generic = func_generic_aoe_corr)
     report = (report_µ = report_µ, report_σ = report_σ)
 
     return result, report
