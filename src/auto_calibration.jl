66f8c3e775422bf53b8de6471e18e383abe484c3
Florian Henkes <florian.henkes@outlook.com>
2024-08-13 17:10:43 +0200

    Bug fixe autocal energy

--- src/auto_calibration.jl
+++ src/auto_calibration.jl
@@ -2,22 +2,13 @@
 
 
 """
-    autocal_energy(e::AbstractArray{<:Real}, photon_lines::Vector{<:Real}; calib_type::Symbol=:th228, kwargs...)
+    autocal_energy(e::AbstractArray{<:Real}, photon_lines::Vector{<:Unitful.RealOrRealQuantity}; min_e::Real=100, max_e_binning_quantile::Real=0.5, σ::Real = 2.0, threshold::Real = 50.0, min_n_peaks::Int = length(photon_lines), max_n_peaks::Int = 4 * length(photon_lines), α::Real = 0.01, rtol::Real = 5e-3)
 
 Compute an energy calibration from raw reconstructed energy deposition values based on a given number of known photon lines which are contained in the spectrum
 """
-function autocal_energy(e::AbstractArray{<:Real}, photon_lines::Vector{<:Unitful.RealOrRealQuantity}, calib_type::Symbol=:th228; kwargs...)
-    if calib_type == :th228
-        return autocal_energy_th228(e, photon_lines; kwargs...)
-    else
-        error("Calibration type not supported")
-    end
-end
-export autocal_energy
-
-function autocal_energy_th228(e::AbstractArray{<:Real}, photon_lines::Vector{<:Unitful.RealOrRealQuantity}; min_e::Real=100, max_e_binning_qunatile::Real=0.5, σ::Real = 2.0, threshold::Real = 50.0, min_n_peaks::Int = length(photon_lines), max_n_peaks::Int = 4 * length(photon_lines), α::Real = 0.01, rtol::Real = 5e-3)
+function autocal_energy(e::AbstractArray{<:Real}, photon_lines::Vector{<:Unitful.RealOrRealQuantity}; min_e::Real=100, max_e_binning_quantile::Real=0.5, σ::Real = 2.0, threshold::Real = 50.0, min_n_peaks::Int = length(photon_lines), max_n_peaks::Int = 4 * length(photon_lines), α::Real = 0.01, rtol::Real = 5e-3)
     # binning based on fd with max cut off at certain quantile
-    max_e_binning = quantile(e, max_e_binning_qunatile)
+    max_e_binning = quantile(e, max_e_binning_quantile)
     bin_width = get_friedman_diaconis_bin_width(filter(x -> min_e < x < max_e_binning, e))
     # get binned uncalibrated histogram
     h_uncal = fit(Histogram, e, min_e:bin_width:maximum(e))
@@ -36,3 +27,4 @@ function autocal_energy_th228(e::AbstractArray{<:Real}, photon_lines::Vector{<:U
     report = (h_cal = h_cal, h_uncal = h_uncal, c = c, c_precal = c_precal, threshold = threshold)
     return result, report
 end
+export autocal_energy
