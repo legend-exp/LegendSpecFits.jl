239bd18f408ec5a37fbf01f9551675868aad9c93
Florian Henkes <florian.henkes@outlook.com>
2025-02-25 19:55:33 +0100

    Bug Fix peakfinder_σ_scaled defintion

--- src/sipm_simple_calibration.jl
+++ src/sipm_simple_calibration.jl
@@ -47,7 +47,7 @@ function sipm_simple_calibration(pe_uncal::Vector{<:Real};
         @debug "Using bin width: $(bin_width_cut_scaled)"
         h_uncal_cut = fit(Histogram, pe_uncal, bin_width_cut_min:bin_width_cut_scaled:initial_max_amp)
         peakfinder_σ_scaled::Int = if peakfinder_σ <= 0.0
-            peakfinder_σ_scaled = round(Int, 2*(cuts_1pe.high - cuts_1pe.max) / bin_width_cut_scaled / (2 * sqrt(2 * log(2))) )
+            round(Int, 2*(cuts_1pe.high - cuts_1pe.max) / bin_width_cut_scaled / (2 * sqrt(2 * log(2))) )
         else
             rount(Int, peakfinder_σ)  
         end
@@ -76,7 +76,7 @@ function sipm_simple_calibration(pe_uncal::Vector{<:Real};
         
         h_uncal_cut = fit(Histogram, pe_uncal, bin_width_cut_min:bin_width_cut_scaled:initial_max_amp)
         peakfinder_σ_scaled::Int = if peakfinder_σ <= 0.0
-            peakfinder_σ_scaled = round(Int, 2*(cuts_1pe.high - cuts_1pe.max) / bin_width_cut_scaled / (2 * sqrt(2 * log(2))) )
+            round(Int, 2*(cuts_1pe.high - cuts_1pe.max) / bin_width_cut_scaled / (2 * sqrt(2 * log(2))) )
         else
             rount(Int, peakfinder_σ)
         end
