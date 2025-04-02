020760b98939754e0081168256038b4b624e5113
LisaSchlueter <lschlueter@lbl.gov>
2025-04-01 17:36:53 -0700

    add IntervalSets for test

--- test/test_simplecal.jl
+++ test/test_simplecal.jl
@@ -8,9 +8,10 @@ using StatsBase
 using Interpolations
 using Unitful 
 using IntervalSets
-include("test_utils.jl")
+# include("test_utils.jl")
 
 @testset "simplecal" begin
+    # Th228 
     # generate fake data - very simplified
     energy_test, th228_lines = generate_mc_spectrum(200000)
     m_cal_simple = 0.123u"keV"
@@ -18,15 +19,30 @@ include("test_utils.jl")
 
     # binning and peak-finder settings that should work for this fake data set 
     window_sizes =  vcat([(25.0u"keV",25.0u"keV") for _ in 1:6], (30.0u"keV",30.0u"keV"))
+    quantile_perc = 0.995
+  
+    # simple calibration
+    result_simple, report_simple = simple_calibration(e_uncal, th228_lines, window_sizes,; calib_type= :th228,  quantile_perc = quantile_perc);
+    @test isapprox(result_simple.c, m_cal_simple, atol = 0.05.*(m_cal_simple))
+
+
+    # Co60 
+    # generate fake data - very simplified
+    energy_test, co60_lines = generate_mc_spectrum_co60(200000)
+    m_cal_simple = 0.123u"keV"
+    e_uncal = energy_test ./ m_cal_simple
+
+    # binning and peak-finder settings that should work for this fake data set 
+    window_sizes =  vcat((25.0u"keV",25.0u"keV"), (30.0u"keV",30.0u"keV"))
     quantile_perc=NaN
     peakfinder_σ = 2.0
-    peakfinder_threshold = 6.0
-    peak_quantile = 0.7..1.0
-    bin_quantile =  0.05..0.5
+    peakfinder_threshold = 50.0
+    peak_quantile = 0.0..1.0
+    bin_quantile =  0.0..0.5
     quantile_perc = NaN 
-    kwargs = (peakfinder_σ=peakfinder_σ, peakfinder_threshold=peakfinder_threshold, peak_quantile=peak_quantile, bin_quantile=bin_quantile, quantile_perc = quantile_perc)
+    kwargs = (peaksearch_type = :most_prominent, peakfinder_σ=peakfinder_σ, peakfinder_threshold=peakfinder_threshold, peak_quantile=peak_quantile, bin_quantile=bin_quantile, quantile_perc = quantile_perc)
     
     # simple calibration
-    result_simple, report_simple = simple_calibration(e_uncal, th228_lines, window_sizes,; calib_type= :th228, kwargs...);
+    result_simple, report_simple = simple_calibration(e_uncal, co60_lines, window_sizes,; calib_type= :gamma, kwargs...);
     @test isapprox(result_simple.c, m_cal_simple, atol = 0.05.*(m_cal_simple))
 end
